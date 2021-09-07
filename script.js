34de6914469236eaa8e0c0a578f8e838f78c58c8
Paulo Guilherme Bacelar Andrade <paulobacelara@gmail.com>
2021-09-07 14:19:24 -0300

    Created nav menu toggle

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // Hide and show nav
 const navButton = document.querySelector("#nav-icon");
+const navMenu = document.querySelector("#nav-menu");
 
 navButton.addEventListener("click", () => {
   navButton.classList.toggle("nav__icon-active");
+  navMenu.classList.toggle("nav__menu-active");
 });
