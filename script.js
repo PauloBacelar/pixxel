c0a96b75efc10c758019bb0d8e4212c6347ba287
Paulo Guilherme Bacelar Andrade <paulobacelara@gmail.com>
2021-09-10 15:09:24 -0300

    Added box-shadow to nav when scroll (mobile)

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
 // Hide and show nav
 const navButton = document.querySelector("#nav-icon");
 const navMenu = document.querySelector("#nav-menu");
+const nav = document.querySelector(".nav");
+const navLinks = Array.from(document.getElementsByClassName("nav__link"));
 
 navButton.addEventListener("click", () => {
   navButton.classList.toggle("nav__icon-active");
   navMenu.classList.toggle("nav__menu-active");
+});
+
+console.log(navLinks);
+
+navLinks.map((link) => {
+  link.addEventListener("click", () => {
+    navMenu.classList.remove("nav__menu-active");
+  });
+});
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 20) {
+    nav.classList.add("nav__box-shadow");
+  } else {
+    nav.classList.remove("nav__box-shadow");
+  }
 });
